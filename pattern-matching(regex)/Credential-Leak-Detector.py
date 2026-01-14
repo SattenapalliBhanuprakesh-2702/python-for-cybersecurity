cb2a54d28e57aefb4db5eb5a456bd837abb0e798
SattenapalliBhanuprakesh-2702 <sattenapallibhanuprakash84@gmail.com>
2026-01-14 20:23:51 +0530

    Credential-leak-detector

---
+++ pattern-matching(regex)/Credential-Leak-Detector.py
@@ -0,0 +1,12 @@
+import re
+
+data="""
+    user=admin pass=123456
+    login=root password=hacked
+"""
+
+cred=re.findall(r"(user|login)=([a-zA-Z0-9_]+).*?(pass|password)=([a-zA-Z0-9]+)",data)
+
+
+for c in cred:
+    print("Username:",c[1],"password:",c[3])
